79d05d80999b5663907db2d8a981f94513a85235
Souvik <souvik131@gmail.com>
2018-03-26 21:08:30 +0530

    msg

--- index.js
+++ index.js
@@ -155,7 +155,7 @@ function filter(request){
 function vaildateSelectedAmc(model) {
     return new Promise(function(resolve, reject) {
         console.log("validate amc" + JSON.stringify(model))
-        var match = stringSimilarity.findBestMatch(model.data, model.amcNamesArray);
+        var match = stringSimilarity.findBestMatch(model.data, model.tags.amcNamesArray);
         if(model.tags.amcConfirmation){
             if(model.data.toLowerCase().includes("yes")){
                 delete model.stage;
