27d1c5f0bc38072da8022fc7717daed4fbbf8862
Souvik <souvik131@gmail.com>
2018-03-27 13:15:33 +0530

    msg

--- index.js
+++ index.js
@@ -171,7 +171,7 @@ function vaildateSelectedAmc(model) {
             if( match
                &&match.bestMatch
                &&match.bestMatch.rating
-               &&match.bestMatch.rating>0){
+               &&match.bestMatch.rating>0.2){
                 console.log("ABOVE THRESHOLD-----------------")
                 model.tags.match=match.bestMatch.target;
             }
