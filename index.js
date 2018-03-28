a1dc6c33f67bc018d72d65538cb1b024535497b6
Souvik <souvik131@gmail.com>
2018-03-28 14:06:52 +0530

    msg

--- index.js
+++ index.js
@@ -195,7 +195,8 @@ function validateSchemeName(model){
                 if(model.tags.schemeCodes.includes(parseInt(model.data.match(/\d+/g)[0]))){
                     for(let i=0;i<model.tags.schemeDetails.length;i++){
                         if(parseInt(model.tags.schemeDetails[i].SCHEMECODE)===parseInt(model.data.match(/\d+/g)[0])){
-                            model.tags.schemeData=model.tags.schemeDetails[i]
+                            model.tags.schemeData=model.tags.schemeDetails[i];
+                            console.log(JSON.stringify(model.tags.schemeData)+"++++++")
                             delete model.stage;
                             break;
                         }
