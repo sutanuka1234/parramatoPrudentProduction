bf02830f460a16007ef97d796f371dce09f1baf4
Souvik <souvik131@gmail.com>
2018-04-04 16:11:41 +0530

    msg

--- stages/subnatureType/post.js
+++ stages/subnatureType/post.js
@@ -5,48 +5,54 @@ module.exports={
 var stringSimilarity = require('string-similarity');
 
 function validateSubnatureOptions(model){
+    console.log("SUBNATURE TYPE POST");
     return new Promise(function(resolve,reject){
         try{
-            if(model.tags.subnatureOptionNames){
-                if(model.tags.confirmSubnature){
-                    if(model.data.toLowerCase().includes("yes")){
-                        for(let i=0;i<model.tags.subnatureOptions.length;i++){
-                            if(model.tags.subnatureMatch===model.tags.subnatureOptions[i].SubNature){
-                                model.tags.subnature=model.tags.subnatureOptions[i].SubNature;
-                                model.tags.subnatureId=model.tags.subnatureOptions[i].ID;
-                                delete model.stage;
-                            }
-                        }  
+            if(model.data.toLowerCase().includes("cancel")){
+                return reject(model);  
+            }
+            else{
+                if(model.tags.subnatureOptionNames){
+                    if(model.tags.confirmSubnature){
+                        if(model.data.toLowerCase().includes("yes")){
+                            for(let i=0;i<model.tags.subnatureOptions.length;i++){
+                                if(model.tags.subnatureMatch===model.tags.subnatureOptions[i].SubNature){
+                                    model.tags.subnature=model.tags.subnatureOptions[i].SubNature;
+                                    model.tags.subnatureId=model.tags.subnatureOptions[i].ID;
+                                    delete model.stage;
+                                }
+                            }  
+                        }
+                        delete model.tags.subnatureMatch;
+                        delete model.tags.confirmSubnature;
                     }
-                    delete model.tags.subnatureMatch;
-                    delete model.tags.confirmSubnature;
-                }
-                else{
-                    var match = stringSimilarity.findBestMatch(model.data, model.tags.subnatureOptionNames);
-                    if(     match
-                       &&   match.bestMatch
-                       &&   match.bestMatch.rating
-                       &&   ((match.bestMatch.rating)==1)){
-                        model.tags.subnatureMatch=match.bestMatch.target;
-                        for(let i=0;i<model.tags.subnatureOptions.length;i++){
-                            if(model.tags.subnatureMatch===model.tags.subnatureOptions[i].SubNature){
-                                model.tags.subnature=model.tags.subnatureOptions[i].SubNature;
-                                model.tags.subnatureId=model.tags.subnatureOptions[i].ID;
-                                delete model.stage;
+                    else{
+                        var match = stringSimilarity.findBestMatch(model.data, model.tags.subnatureOptionNames);
+                        if(     match
+                           &&   match.bestMatch
+                           &&   match.bestMatch.rating
+                           &&   ((match.bestMatch.rating)==1)){
+                            model.tags.subnatureMatch=match.bestMatch.target;
+                            for(let i=0;i<model.tags.subnatureOptions.length;i++){
+                                if(model.tags.subnatureMatch===model.tags.subnatureOptions[i].SubNature){
+                                    model.tags.subnature=model.tags.subnatureOptions[i].SubNature;
+                                    model.tags.subnatureId=model.tags.subnatureOptions[i].ID;
+                                    delete model.stage;
+                                }
                             }
                         }
-                    }
-                    else if (match
-                       &&   match.bestMatch
-                       &&   match.bestMatch.rating
-                       &&   (match.bestMatch.rating>0)
-                       &&   (match.bestMatch.rating<1)){
-                        model.tags.subnatureMatch=match.bestMatch.target;
-                        model.tags.reaffirm=match.bestMatch.target;
+                        else if (match
+                           &&   match.bestMatch
+                           &&   match.bestMatch.rating
+                           &&   (match.bestMatch.rating>0)
+                           &&   (match.bestMatch.rating<1)){
+                            model.tags.subnatureMatch=match.bestMatch.target;
+                            model.tags.reaffirm=match.bestMatch.target;
+                        }
                     }
                 }
+                return resolve(model);
             }
-            return resolve(model);
         }
         catch(e){
             console.log(e);
--- stages/subnatureType/pre.js
+++ stages/subnatureType/pre.js
@@ -3,6 +3,7 @@ module.exports={
 }
 
 function getSubnatureOptions(model){
+    console.log("SUBNATURE TYPE PRE");
     return new Promise(function(resolve,reject){
         try{
             if(model.tags.reaffirm){
@@ -25,37 +26,43 @@ function getSubnatureOptions(model){
             }
             else{
                 if(model.tags.subnatureOptions){
-                    let reply={};
-                    reply.type="generic";
-                    reply.text="Please choose from the following sub-natures."
-                    reply.next={
-                        data: []
-                    }
-
-                    let loop=model.tags.subnatureOptions.length/3;
-
-                    let min=0;
-                    let max=3
-
-                    for(let i=0;i<Math.ceil(loop);i++){
-                        reply.next.data.push({
-                            title   :"Select from the following Sub-natures.",
-                            text    :"",
-                            buttons :[]
-                        })
-                        for(let j=min;j<max;j++){
-                            if(     model.tags.subnatureOptions[j]
-                               &&   model.tags.subnatureOptions[j].SubNature){
-                                reply.next.data[i].buttons.push({
-                                    text:model.tags.subnatureOptions[j].SubNature,
-                                    data:model.tags.subnatureOptions[j  ].SubNature
-                                })
-                            }
+                    if(!model.tags.subnatureCards){
+                        let reply={};
+                        reply.type="generic";
+                        reply.text="Please choose from the following sub-natures."
+                        reply.next={
+                            data: []
                         }
-                        min=max;
-                        max=max+3;
+
+                        let loop=model.tags.subnatureOptions.length/3;
+
+                        let min=0;
+                        let max=3
+
+                        for(let i=0;i<Math.ceil(loop);i++){
+                            reply.next.data.push({
+                                title   :"Select from the following Sub-natures.",
+                                text    :"",
+                                buttons :[]
+                            })
+                            for(let j=min;j<max;j++){
+                                if(     model.tags.subnatureOptions[j]
+                                   &&   model.tags.subnatureOptions[j].SubNature){
+                                    reply.next.data[i].buttons.push({
+                                        text:model.tags.subnatureOptions[j].SubNature,
+                                        data:model.tags.subnatureOptions[j  ].SubNature
+                                    })
+                                }
+                            }
+                            min=max;
+                            max=max+3;
+                        }
+                        model.tags.subnatureCards=reply;
+                        model.reply=reply;
                     }
-                    model.reply=reply;
+                    else{
+                        model.reply=model.tags.subnatureCards;
+                    }
                 }
             }
             return resolve(model);
