78354197a0b824eefa9afed5d6888074f148e12f
Souvik <souvik131@gmail.com>
2018-04-04 19:11:30 +0530

    msg

--- stages/folio/post.js
+++ stages/folio/post.js
@@ -13,12 +13,14 @@ function validateFolio(model){
 //                console.log(JSON.stringify(model.tags.foliosArray)+"//");
                 if(model.tags.foliosArray.includes(parseInt(model.data.match(/\d+/g)[0]))){
                     model.tags.folioSelected=model.data.match(/\d+/g)[0]
-                    delete model.stage;    
+                    delete model.stage;  
+                    return resolve(model);
                 }
             }
             else if(model.data.toLowerCase().includes("new folio")){
                 model.tags.folioSelected="New Folio";
                 delete model.stage;
+                return resolve(model);
             }
             else{
                 let reply=model.tags.folioCards;
