b73ece1cefb6069cc1e12e4ec1e0cbabcb891dfb
Souvik <souvik131@gmail.com>
2018-04-04 18:48:10 +0530

    msg

--- stages/folio/post.js
+++ stages/folio/post.js
@@ -3,11 +3,11 @@ module.exports={
 };
 
 function validateFolio(model){
-    console.log("FOLIO POST");
+    //console.log("FOLIO POST");
     return new Promise(function(resolve,reject){
         try{
             if(model.data.match(/\d+/g)){
-//                console.log(JSON.stringify(model.tags.foliosArray)+"//");
+//                //console.log(JSON.stringify(model.tags.foliosArray)+"//");
                 if(model.tags.foliosArray.includes(parseInt(model.data.match(/\d+/g)[0]))){
                     model.tags.folioSelected=model.data.match(/\d+/g)[0]
                     delete model.stage;    
@@ -22,10 +22,9 @@ function validateFolio(model){
             else{
                 return reject(model);
             }
-            
         }
         catch(e){
-            console.log(e);
+            //console.log(e);
             return reject("Something went wrong.");
         }
     })
