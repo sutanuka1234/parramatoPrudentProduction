808b0903432605acc81a2e65e5bf4d06770ceedb
Souvik <souvik131@gmail.com>
2018-04-05 16:33:40 +0530

    msg

--- stages/mandate/post.js
+++ stages/mandate/post.js
@@ -6,12 +6,12 @@ function validateMandate(model){
     return new Promise(function(resolve,reject){
         try{
             console.log(model.data+"USER SAID AT MANDATE")
-            if(     model.data.match(/NFB\d{7})
-              &&    model.data.match(/NFB\d{7})[0]
-              &&    model.data.match(/NFB\d{7})[0].toString().length===10
-              &&    model.tags.TGEditMndID.includes(model.data.match('/NFB\d{7}')[0])){
-                console.log(model.data.match('/NFB\d{7}')[0]+"TGEditMndID");
-                let mandateIdTemp=model.data.match('/NFB\d{7}')[0];
+            if(     model.data.match(/NFB\d{7}/)
+              &&    model.data.match(/NFB\d{7}/)[0]
+              &&    model.data.match(/NFB\d{7}/)[0].toString().length===10
+              &&    model.tags.TGEditMndID.includes(model.data.match(/NFB\d{7}/)[0])){
+                console.log(model.data.match(/NFB\d{7}/)[0]+"TGEditMndID");
+                let mandateIdTemp=model.data.match(/NFB\d{7}/)[0];
                 for(let i=0;i<model.tags.mandateDetails.length;i++){
                     if(model.tags.mandateDetails[i].TGEditMndID===mandateIdTemp){
                         model.tags.selectedMandate=model.tags.mandateDetails[i];
