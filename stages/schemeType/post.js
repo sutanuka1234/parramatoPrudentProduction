66256748b95b5cd7bab15e962dcb39c4b78a8c14
Souvik <souvik131@gmail.com>
2018-04-11 16:18:27 -0700

    msg

--- stages/schemeType/post.js
+++ stages/schemeType/post.js
@@ -8,17 +8,22 @@ function validateSchemeType(model){
     return new Promise(function(resolve,reject){
         try{
         	 console.log("*********************")
-        	 console.log("Type " + model.tags.showOptions)
+        	 console.log("Type " + JSON.stringify(model.tags.showOptions))
         	    let containsMatch;
         	 	for(let typeIndex; typeIndex < model.tags.showOptions; typeIndex++) {
+        	 		console.log("*********************")
+        	 		console.log("model.tags.showOptions[typeIndex].toLowerCase()" + model.tags.showOptions[typeIndex].toLowerCase())
+        	 		console.log("*********************")
         	 		if(model.data.toLowerCase().includes(model.tags.showOptions[typeIndex].toLowerCase())){
         	 			containsMatch = true
         	 		} 
         	 	}
         	    if(containsMatch) {
+        	    	console.log("containsMatch" + containsMatch)
 	        	 	delete model.stage;
 	        	 	return resolve(model);
         	    } else {
+        	    	console.log("containsMatch" + containsMatch)
         	    	return resolve(model)
         	    }
 
