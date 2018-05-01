a539d24c7f18653268b808d24f179d9165060a4f
Souvik <souvik@jubination.com>
2018-05-01 18:58:01 +0530

    msg

--- purchase/post.js
+++ purchase/post.js
@@ -711,7 +711,7 @@ function folio(model){
 			console.log(schemes[model.tags.scheme].amcCode)
 			console.log(model.tags.divOps)
 			console.log(model.tags.amount)
-			api.insertBuyCart(model.tags.session, '334', schemes[model.tags.scheme].schemeCode, 'Axis Asset Management Company Ltd.', schemes[model.tags.scheme].amcCode, '0', model.tags.amount, '0')
+			api.insertBuyCart('7C772321713D21713D21713D21713D21713D21713D21713D3F63263F6326', '334', '8408', 'Axis Asset Management Company Ltd.', '400040', '0', '10000', '0')
 					.then((data)=>{
 						console.log(data.body)
 						data = JSON.parse(data)
