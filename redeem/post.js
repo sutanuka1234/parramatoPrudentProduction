a198f3c8ce0c2161a50f15bb8f3ad31d001d5a95
Souvik Das <souvik131@gmail.com>
2018-05-16 13:24:31 +0530

    msg

---
+++ redeem/post.js
@@ -0,0 +1,704 @@
+module.exports={
+	main:main
+}
+
+let api = require('../api.js')
+let external = require('../external.js')
+let words = require('../words.js')
+let data = require('../data.js')
+let stringSimilarity = require('string-similarity');
+let sortBy = require('sort-by')
+let matchAll = require('match-all')
+
+let obj = {
+	panMobile : panMobile,
+	mobile	: mobile,
+	pan		: pan,
+	otp		: otp,
+	askSchemeName : askSchemeName,
+	showSchemeName : showSchemeName,
+	divOps 	: divOps,
+	amount 	: amount,
+	holding : holding,
+	additional : additional,
+	folio 	: folio,
+	bankMandate : bankMandate
+}
+
+
+let regexMobile	= /[789]\d{9}/gi
+let regexPan 	= /[a-z]{3}p[a-z]\d{4}[a-z]/
+let number		= /\d+/
+let regexAmount	= /(\d{7}|\d{6}|\d{5}|\d{4}|\d{3}|\d{2}(k|l)|\d{1}(k|l))/gi
+let divOption 	= /re(-|\s)?invest|pay(\s)?out/
+let regexFolio 	= /i?\s*(have|my)?\s*a?\s*folio\s*(n(umber|um|o)?)?\s*(is|=|:)?\s*(\d+|new folio)/
+let schemeType 	= /dividend|growth/
+let regexOtp    = /\d{6}/
+let schemeNames = Object.keys(data)
+let amc = [  
+	'kotak',
+	'birla',
+	'sun life',
+	'aditya',
+	'sundaram',
+	'sbi',
+	'uti',
+	'dsp',
+	'black rock',
+	'blackrock',
+	'franklin',
+	'templeton',
+	'tata',
+	'reliance',
+	'idbi',
+	'icici',
+	'hdfc',
+	'lic',
+	'axis',
+	'l&t',
+	'lnt',
+	'l and t',
+	'bnp',
+	'paribas',
+	'baroda',
+	'pioneer',
+	'idfc',
+	'invesco',
+	'boi',
+	'axa',
+	'canara',
+	'robeco',
+	'dhfl',
+	'pramerica',
+	'mirae',
+	'mahindra',
+	'motilal',
+	'oswal',
+	'principal pnb'
+]
+
+function main(req, res){
+		// console.log(req.params.stage)
+		obj[req.params.stage](req.body)
+		.then((data)=>{
+			// console.log(req.params.stage+"::::::::::::::::::::::::::::::::::::::::::")
+			// console.log(JSON.stringify(data.tags,null,3))
+			res.send(data)
+		})
+		.catch((e)=>{
+			// console.log(e)
+			res.sendStatus(203)
+		})
+}
+
+//============================================================
+
+function panMobile(model){
+	return new Promise(function(resolve, reject){
+		model=dataClean(model);
+		if(model.data.toLowerCase().includes("not")&&model.data.toLowerCase().includes("me")){
+			model.stage="panMobile";
+			model.tags={}
+			return resolve(model);
+		}
+		model = extractPan(model);
+		if(model.tags.newPan){
+			let temp = {pan:model.tags.pan}
+			if(model.tags.newFolio){
+				// console.log("FOLIO")
+				temp.folio=model.tags.folio;
+			}
+			if(model.tags.newScheme){
+				// console.log("SCHEME")
+				temp.schemes=model.tags.schemes;
+			}
+			if(model.tags.newAmount){
+				// console.log("AMOUNT")
+				temp.amount=model.tags.amount;
+			}
+			if(model.tags.newDivOption){
+				// console.log("DIVOPTION")
+				temp.divOption=model.tags.divOption;
+			}
+			model.tags=temp;
+			model.tags.newPan=undefined;
+			model.tags.newFolio=undefined;
+			model.tags.newScheme=undefined;
+			model.tags.newAmount=undefined;
+			model.tags.newDivOption=undefined;
+		}
+		else{
+				model.tags.newFolio=undefined;
+				model.tags.newScheme=undefined;
+				model.tags.newAmount=undefined;
+				model.tags.newDivOption=undefined;
+
+		}
+		if(model.data&&!model.data.includes("proceed")&&model.tags.mobile&&model.tags.pan){	
+			// console.log("1")
+			return reject(model);
+		}
+		else if(model.data&&model.data.includes("proceed")&&model.tags.mobile&&model.tags.pan){
+			// console.log("2")
+			api.panMobile(model.tags.mobile, model.tags.pan)
+			.then(data=>{
+				// console.log("then")
+				// console.log(data.body)
+				let response;
+				try{
+					response = JSON.parse(data.body)
+				}
+				catch(e){
+					// console.log(e);
+					if(!model.tags.mobile){
+						model.stage = 'mobile' 
+						return resolve(model)
+					}
+					else if(!model.tags.pan){
+						model.stage = 'pan' 
+						return resolve(model)
+					}		
+					return reject(model);
+				}
+				if(response.Response[0].result=="FAIL"){
+					if(response.Response[0]['reject_reason']=="Client does not exists."){
+						response.Response[0]['reject_reason']="Your pan and mobile combination does not seem to be valid."
+					}
+					let reply={
+		                text    : response.Response[0]['reject_reason'],
+		                type    : "text",
+		                sender  : model.sender,
+		                language: "en"
+		            }
+					external(reply)
+					.then((data)=>{ 
+		                model.tags={}
+						return resolve(model)
+		            })
+		            .catch((e)=>{
+		                // console.log(e);
+		                if(!model.tags.mobile){
+							model.stage = 'mobile' 
+							return resolve(model)
+						}
+						else if(!model.tags.pan){
+							model.stage = 'pan' 
+							return resolve(model)
+						}		
+		                return reject(model)
+		            })
+				}
+				else{
+					model.tags.session = response.Response[0].SessionId
+					model.stage = 'otp' 
+					return resolve(model)
+				}
+			})
+			.catch(error=>{
+				// console.log("catch")
+				// console.log(error);
+				if(!model.tags.mobile){
+					model.stage = 'mobile' 
+					return resolve(model)
+				}
+				else if(!model.tags.pan){
+					model.stage = 'pan' 
+					return resolve(model)
+				}		
+				return reject(model)
+			})		
+		}
+		else if(model.data&&model.data.includes("proceed")&&(model.tags.mobile||model.tags.pan)){
+			// console.log("3")
+
+			if(!model.tags.mobile&&!model.tags.pan){
+				return reject(model);
+			}
+			if(!model.tags.mobile){
+				model.stage = 'mobile' 
+				return resolve(model)
+			}
+			else if(!model.tags.pan){
+				model.stage = 'pan' 
+				return resolve(model)
+			}
+		}
+		else{ 
+			// console.log("4")
+			model = extractMobile(model);
+			model = extractDivOption(model);
+			model = extractSchemeName(model);
+			model = extractAmount(model);
+			model = extractFolio(model);
+			if(model.tags.pan&&model.tags.mobile){
+				api.panMobile(model.tags.mobile, model.tags.pan)
+				.then(data=>{
+					// console.log(data.body)
+					let response;
+					try{
+						response = JSON.parse(data.body)
+					}
+					catch(e){// console.log(e);
+						if(!model.tags.mobile){
+							model.stage = 'mobile' 
+							return resolve(model)
+						}
+						else if(!model.tags.pan){
+							model.stage = 'pan' 
+							return resolve(model)
+						}		
+						return reject(model);
+					}
+					if(response.Response[0].result=="FAIL"){
+						if(response.Response[0]['reject_reason']=="Client does not exists."){
+							response.Response[0]['reject_reason']="Your pan and mobile combination does not seem to be valid."
+						}
+						let reply={
+			                text    : response.Response[0]['reject_reason'],
+			                type    : "text",
+			                sender  : model.sender,
+			                language: "en"
+			            }
+						external(reply)
+						.then((data)=>{  
+		                	model.tags={};
+							return resolve(model)
+			            })
+			            .catch((e)=>{
+			                // console.log(e);
+			                if(!model.tags.mobile){
+								model.stage = 'mobile' 
+								return resolve(model)
+							}
+							else if(!model.tags.pan){
+								model.stage = 'pan' 
+								return resolve(model)
+							}		
+			                return reject(model)
+			            })
+					}
+					else{
+						model.tags.session = response.Response[0].SessionId
+						model.stage = 'otp' 
+						return resolve(model)
+					}
+				})
+				.catch(error=>{
+					// console.log(error);
+					if(!model.tags.mobile){
+						model.stage = 'mobile' 
+						return resolve(model)
+					}
+					else if(!model.tags.pan){
+						model.stage = 'pan' 
+						return resolve(model)
+					}		
+					return reject(model)
+				})		
+			}
+			else{
+				if(!model.tags.mobile&&!model.tags.pan){
+					return reject(model);
+				}
+				if(!model.tags.mobile){
+					model.stage = 'mobile' 
+					return resolve(model)
+				}
+				else if(!model.tags.pan){
+					model.stage = 'pan' 
+					return resolve(model)
+				}		
+				return reject(model);
+			}
+		}
+	})
+}
+
+function mobile(model){
+	return new Promise(function(resolve, reject){
+		    model=dataClean(model);
+			model = extractMobile(model);
+			model = extractDivOption(model);
+			model=extractSchemeName(model);
+			model = extractAmount(model);
+			model = extractFolio(model);
+			if(model.tags.pan&&model.tags.mobile){
+					api.panMobile(model.tags.mobile, model.tags.pan)
+					.then(data=>{
+						// console.log(data.body)
+						let response;
+						try{
+							response = JSON.parse(data.body)
+						}
+						catch(e){// console.log(e);
+							return reject(model);
+						}
+						if(response.Response[0].result=="FAIL"){
+							if(response.Response[0]['reject_reason']=="Client does not exists."){
+								response.Response[0]['reject_reason']="Your pan and mobile combination does not seem to be valid."
+							}
+							let reply={
+				                text    : response.Response[0]['reject_reason'],
+				                type    : "text",
+				                sender  : model.sender,
+				                language: "en"
+				            }
+							external(reply)
+							.then((data)=>{
+				                model.stage = 'panMobile'  
+		                		model.tags={};
+								return resolve(model)
+				            })
+				            .catch((e)=>{
+				                // console.log(e);
+				                return reject(model)
+				            })
+						}
+						else{
+							model.tags.session = response.Response[0].SessionId
+							model.stage = 'otp' 
+							return resolve(model)
+						}
+					})
+					.catch(error=>{
+						// console.log(error);
+						return reject(model)
+					})		
+			}
+
+			else if(model.tags.mobile){
+				model.stage = 'pan' 
+				return resolve(model)
+			}
+			else{
+				return reject(model);
+			}
+	
+	})
+}
+
+function pan(model){
+	return new Promise(function(resolve, reject){
+		model = dataClean(model);
+		model = extractPan(model);
+		model = extractDivOption(model);
+		model=extractSchemeName(model);
+		model = extractAmount(model);
+		model = extractFolio(model);
+			// console.log("TAGG")
+			// console.log(JSON.stringify(model.tags,null,3))
+		if(model.tags.pan&&model.tags.mobile){
+			api.panMobile(model.tags.mobile, model.tags.pan)
+			.then(data=>{
+				// console.log(data.body)
+				let response;
+				try{
+					response = JSON.parse(data.body)
+				}
+				catch(e){// console.log(e);
+					return reject(model);
+				}
+				if(response.Response[0].result=="FAIL"){
+					if(response.Response[0]['reject_reason']=="Client does not exists."){
+						response.Response[0]['reject_reason']="Your pan and mobile combination does not seem to be valid."
+					}
+					let reply={
+		                text    : response.Response[0]['reject_reason'],
+		                type    : "text",
+		                sender  : model.sender,
+		                language: "en"
+		            }
+					external(reply)
+					.then((data)=>{
+		                model.stage = 'panMobile' 
+		                model.tags={};
+						return resolve(model)
+		            })
+		            .catch((e)=>{
+		                // console.log(e);
+		                return reject(model)
+		            })
+				}
+				else{
+					model.tags.session = response.Response[0].SessionId
+					model.stage = 'otp' 
+					return resolve(model)
+				}
+			})
+			.catch(error=>{
+				// console.log(error);
+				return reject(model)
+			})		
+		}
+		else if(model.tags.pan){
+			model.stage = 'mobile' 
+			return resolve(model)
+		}
+		else{
+			return reject(model);
+		}
+	})
+}
+
+function otp(model){
+	return new Promise(function(resolve, reject){
+
+		// model.tags.mobileEntered=false;
+		model = dataClean(model);
+		model = extractOTP(model);
+		model = extractDivOption(model);
+		model = extractSchemeName(model);
+		if(model.data.toLowerCase().includes('re send')||model.data.toLowerCase().includes('resend')){
+			api.resendOtp(model.tags.session)
+			.then((response)=>{
+				// console.log(response.body)
+				try{
+					response = JSON.parse(response.body)
+				}
+				catch(e){
+					// console.log(e)
+					reject(model)
+				}
+				if(response.Response){
+					model.tags.resend = true
+					resolve(model)
+				}
+				else{
+					reject(model)
+				}
+			})
+			.catch(e=>{
+				// console.log(e)
+				reject(model)
+			})
+		}
+		else if(model.tags.otp){
+			api.otp(model.tags.session, model.tags.otp)
+			.then(data=>{
+				try{
+					// console.log(data.body)
+					let response;
+					try{
+						response = JSON.parse(data.body)
+					}
+					catch(e){// console.log(e);
+						return reject(model);
+					}
+					if(response.Response[0].result=="FAIL"){
+						let reply={
+			                text    : response.Response[0]['reject_reason'],
+			                type    : "text",
+			                sender  : model.sender,
+			                language: "en"
+			            }
+						external(reply)
+						.then((data)=>{
+							return reject(model)//wrongResolve
+			            })
+			            .catch((e)=>{
+			                // console.log(e);
+			                return reject(model)
+			            })
+					}
+					else{
+						model.tags.joinAcc = response.Response
+						model.tags.joinAccId = []
+						response.Response.forEach(function(element){
+							model.tags.joinAccId.push(element.JoinAccId.toString())
+						})
+						model.tags.joinAccList = []
+						for(let i in model.tags.joinAcc){
+							model.tags.joinAccList.push({
+								title: 'Holding Patterns',
+								text : model.tags.joinAcc[i].JoinHolderName,
+								buttons : [{
+									data : model.tags.joinAcc[i].JoinAccId,
+									text : 'Select'
+								}]
+							})
+						}
+						if(model.tags.schemes && model.tags.schemes.length > 0){
+							model.tags.schemeList = []
+							for(let element of model.tags.schemes){
+								model.tags.schemeList.push({
+									title 	: 'Schemes',
+									text 	: element.target,
+									buttons : [
+										{
+											text : 'Select',
+											data : element.target
+										}
+									]
+								})
+							}
+						}
+						delete model.stage
+						return resolve(model)
+					}
+				}
+				catch(e){
+					// console.log(e);
+					return reject(model);
+				}
+			})
+			.catch(error=>{
+				// console.log(error);
+				return reject(model)
+			})
+		}
+		else{
+			return reject(model)
+		}
+	})
+}
+
+//============================================================
+
+function sendExternalMessage(model,text){
+	let reply={
+            text    : text,
+            type    : "text",
+            sender  : model.sender,
+            language: "en"
+        }
+		external(reply)
+		.then((data)=>{ })
+        .catch((e)=>{
+            // console.log(e);
+       })
+}
+
+function extractOTP(model){
+	if(model.data.match(regexOtp)){
+		model.tags.otp = model.data.match(regexOtp)[0]
+		model.data = model.data.replace(model.tags.regexOtp, '')
+	}
+	return model;
+}
+
+function extractDivOption(model){
+	if(model.data.match(divOption)){
+		model.tags.divOption = model.data.match(divOption)[0]
+		model.data = model.data.replace(model.tags.divOption, '')
+	}
+	return model;			
+}
+
+function extractFolio(model){
+	if(model.data.match(regexFolio)){
+
+		model.tags.folio = model.data.match(regexFolio)[0].match(/\d+|new folio/)[0]
+		model.data = model.data.replace(model.tags.folio, '')
+	}
+	return model;
+}
+
+function extractAmount(model){
+	if(model.data.includes(',')){
+		while(model.data.includes(','))
+    		model.data = model.data.replace(',', '')
+	}
+	if(model.data.match(/\d+\s*k/)){
+		let a = model.data
+   		a = a.match(/\d+\s*k/)[0].replace(/\s+/, '').replace('k', '000')
+   		model.data = model.data.replace(/\d+\s*k/, a)
+	}
+    if(model.data.match(/\d+(\s*)?(lakhs|lakh|lacs|l)/)){
+    	let a = model.data
+		a = a.match(/\d+\s*(lakhs|lakh|lacs|l)/)[0].replace(/\s+/, '').replace('lakhs', '00000').replace('lakh', '00000').replace('lacs', '00000').replace('l', '00000')
+    	model.data = model.data.replace(/\d+\s*(lakhs|lakh|lacs|l)/, a)
+    }
+	let text = matchAll(model.data, /(\d+)/gi).toArray()
+	for(let i in text){
+		if(text[i].length < 8){
+			model.tags.amount = text[i]
+			model.data = model.data.replace(model.tags.amount, '')
+			break;
+		}
+	}
+	return model;
+}
+
+function extractMobile(model){
+	let text = matchAll(model.data, /(\d+)/gi).toArray()
+
+	// console.log(text)
+	for(let i in text){
+		if(text[i].length == 10){
+			model.tags.mobile = text[i]
+			model.data = model.data.replace(model.tags.mobile, '')
+			// console.log(model.tags.mobile+"mobile")
+			break;
+		}
+	}
+	return model;
+}
+
+function extractPan(model){
+	let matchPan=model.data.match(regexPan)
+	// console.log(matchPan)
+	if(matchPan&&matchPan.length>0&&matchPan[0]){
+
+		if(matchPan[0]!=model.tags.pan){
+			model.tags.newPan = true;
+		}
+		model.tags.pan = matchPan[0]
+		model.data=model.data.replace(model.tags.pan, '')
+		// console.log(model.tags.pan+"pan")
+	}
+	return model;
+}
+
+function extractSchemeName(model){
+		let wordsInUserSays=model.data.split(" ");
+		let count=0;
+		let startIndex;
+		let endIndex;
+		let amcIndex;
+		let amcFlag;
+		for(let wordIndex in wordsInUserSays){
+			if(words.includes(wordsInUserSays[wordIndex])){
+				count++;
+				if(count==1){
+					startIndex=wordIndex;
+					endIndex=wordIndex;
+				}
+				else{
+					endIndex=wordIndex;
+				}
+			}
+			if(amc.includes(wordsInUserSays[wordIndex])&&!amcFlag){
+				amcIndex=wordIndex;
+				amcFlag=true;
+			}
+		}
+		if(amcFlag){
+			// 	startIndex=amcIndex
+			// }
+
+			if(count>0){
+				let searchTerm=""
+				for(let i=parseInt(startIndex);i<=parseInt(endIndex);i++){
+					searchTerm+=wordsInUserSays[i]+" "
+				}
+				searchTerm=searchTerm.trim();
+				// console.log(searchTerm)
+				let matches = stringSimilarity.findBestMatch(searchTerm, schemeNames)
+				if(matches.bestMatch.rating>0.9){
+					model.tags.schemes = []
+					model.tags.schemes.push(matches.bestMatch.target)
+				}
+				else if(matches.bestMatch.rating>0.4){
+					model.tags.schemes = []
+					matches.ratings=matches.ratings.sort(sortBy('-rating'));
+					model.tags.schemes = matches.ratings.splice(0,9);
+				}
+			}
+		}
+		return model;
+}
+
+function dataClean(model){
+	model.data = model.data.toLowerCase()
+    return model;
+}
