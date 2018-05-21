bca65da816523f2616da6cccbe240cb0aaf88441
Souvik Das <souvik131@gmail.com>
2018-05-21 17:10:07 +0530

    msg

--- redeem/post.js
+++ redeem/post.js
@@ -555,7 +555,7 @@ function holding(model){
 			console.log("here")
 			api.getFolio(model.tags.session, model.data, data[model.tags.scheme].schemeCode, data[model.tags.scheme].amcCode)
 			.then(response=>{
-				console.log(response.body)
+				console.log(response)
 				try{
 					response = JSON.parse(response.body)
 					if(response.Response.length > 0){
