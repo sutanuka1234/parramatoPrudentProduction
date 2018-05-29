b3a7527039e023d51c85686d9442b30ca0c53e55
Souvik Das <souvik131@gmail.com>
2018-05-29 17:30:57 +0530

    msg

--- selectFallback/pre.js
+++ selectFallback/pre.js
@@ -31,60 +31,66 @@ function main(req, res){
 
 function fallback(model){
 	return new Promise((resolve,reject)=>{
-		console.log(JSON.stringify(model.bestIntents,null,3)+":::::::::::::::::>>>>>>>>>>>>>>>>>")
-		for(let index in model.bestIntents){
-			if(model.bestIntents[index].intentName.startsWith("st_")){
-				model.bestIntents.splice(index, 1)
+		try{
+			console.log(JSON.stringify(model.bestIntents,null,3)+":::::::::::::::::>>>>>>>>>>>>>>>>>")
+			for(let index in model.bestIntents){
+				if(model.bestIntents[index].intentName.startsWith("st_")){
+					model.bestIntents.splice(index, 1)
+				}
 			}
-		}
-		if(model.prevQuery){
-			if(model.prevQuery.startsWith("<similar> ")){
-				model.prevQuery=model.prevQuery.replace("<similar>").replace("</similar>").trim().split("|")[0];
+			if(model.prevQuery){
+				if(model.prevQuery.startsWith("<similar> ")){
+					model.prevQuery=model.prevQuery.replace("<similar>").replace("</similar>").trim().split("|")[0];
+				}
+				if(model.prevQuery.startsWith("<nomatch> ")){
+					model.prevQuery=model.prevQuery.replace("<similar>").replace("</similar>").trim().split("|")[0];
+				}
 			}
-			if(model.prevQuery.startsWith("<nomatch> ")){
-				model.prevQuery=model.prevQuery.replace("<similar>").replace("</similar>").trim().split("|")[0];
+			let data=[]
+			for(let element of model.bestIntents){
+				let query=replaceAll(element.query,"@","");
+				if(query.length>80){
+					query=query.substring(0, 80)+"...";
+				}
+				if(element.reply.length>80){
+					element.reply=element.reply.substring(0, 80)+"...";
+				}
+				data.push({
+						// title 	: "Query",
+						// text 	: query,
+						title 		: query,
+						text		: element.reply,
+						buttons : [
+							{
+								text : 'Read more',
+								data : "<similar> "+query+"|"+model.prevQuery+"|"+element.id+" </similar>"
+							}
+						]
+					})
 			}
-		}
-		let data=[]
-		for(let element of model.bestIntents){
-			let query=replaceAll(element.query,"@","");
-			if(query.length>80){
-				query=query.substring(0, 80)+"...";
-			}
-			if(element.reply.length>80){
-				element.reply=element.reply.substring(0, 80)+"...";
-			}
-			data.push({
-					// title 	: "Query",
-					// text 	: query,
-					title 		: query,
-					text		: element.reply,
-					buttons : [
-						{
-							text : 'Read more',
-							data : "<similar> "+query+"|"+model.prevQuery+"|"+element.id+" </similar>"
-						}
-					]
+			if(data.length>0){
+				data[data.length-1].buttons.push({
+					text : 'None relevant',
+					data : "<nomatch> "+model.prevQuery+"|"+model.bestIntents[0].id.split("-")[0]+" </nomatch>"
 				})
-		}
-		if(data.length>0){
-			data[data.length-1].buttons.push({
-				text : 'None relevant',
-				data : "<nomatch> "+model.prevQuery+"|"+model.bestIntents[0].id.split("-")[0]+" </nomatch>"
-			})
-			model.reply={
-				type:"generic",
-	            text:"We have got few similar questions asked by people like you. Slide through the cards to find one.",
-	            next:{
-	                data: data
-	            }
+				model.reply={
+					type:"generic",
+		            text:"We have got few similar questions asked by people like you. Slide through the cards to find one.",
+		            next:{
+		                data: data
+		            }
+				}
+			}
+			else{
+				model.reply={
+					type:"generic",
+		            text:"I am not sure how to answer this, trying my best to learn. Could you please rephrase your query?"
+				}
 			}
 		}
-		else{
-			model.reply={
-				type:"generic",
-	            text:"I am not sure how to answer this, trying my best to learn. Could you please rephrase your query?"
-			}
+		catch(e){
+			console.log(e)
+			return reject(model)
 		}
 		return resolve(model)
 	});
