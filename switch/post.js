877488f34a49026ffb4009fa6136cbcee78760b1
Souvik Das <souvik@jubi.ai>
2018-06-20 20:44:41 +0530

    msg

--- switch/post.js
+++ switch/post.js
@@ -1109,8 +1109,8 @@ function euin(model){
 				if(model.tags.divOption!=undefined){
 					model.stage = 'unitOrAmount'
 				}
-				// if(model.stage=="unitOrAmount"&&parseFloat(model.tags.switchSchemeObj["AvailableUnits"])<=1){
-				if(true){
+				if(model.stage=="unitOrAmount"&&parseFloat(model.tags.switchSchemeObj["AvailableUnits"])<=1){
+				// if(true){
 							model.tags.unitOrAmount="AU";
 							// console.log("amount valid")
 							api.insertBuyCartSwitch(model.tags.session, model.tags.joinAccId, model.tags.switchSchemeObj["SCHEMECODE"], data[model.tags.scheme].schemeCode,model.tags.unitOrAmount, model.tags.switchSchemeObj["AvailableUnits"], model.tags.folio,model.tags.divOption,model.tags.euin)
@@ -1192,8 +1192,8 @@ function divOps(model){
 				model.tags.divOptionText="Payout Option"
 			}
 			sendExternalMessage(model,"Going ahead with "+model.tags.divOptionText)
-			// if(parseFloat(model.tags.switchSchemeObj["AvailableUnits"])<=1){
-			if(true){
+			if(parseFloat(model.tags.switchSchemeObj["AvailableUnits"])<=1){
+			// if(true){
 							model.tags.unitOrAmount="AU";
 							// console.log("amount valid")
 							api.insertBuyCartSwitch(model.tags.session, model.tags.joinAccId, model.tags.switchSchemeObj["SCHEMECODE"], data[model.tags.scheme].schemeCode,model.tags.unitOrAmount, model.tags.switchSchemeObj["AvailableUnits"], model.tags.folio,model.tags.divOption,model.tags.euin)
