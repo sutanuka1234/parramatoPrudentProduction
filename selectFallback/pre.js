1020c6e5afb11be4d3633015ce72c2a4f60672de
Souvik Das <souvik@jubi.ai>
2018-07-26 12:26:28 +0530

    msg

--- selectFallback/pre.js
+++ selectFallback/pre.js
@@ -41,7 +41,7 @@ function fallback(model){
 				catch(e){
 					console.log(e)
 				}
-				if(model.bestIntents[index].intentName.startsWith("st_")&&confidence<0.30){
+				if(model.bestIntents[index].intentName.startsWith("st_")||confidence<0.30){
 					// model.bestIntents.splice(index, 1)
 				}
 				else{
