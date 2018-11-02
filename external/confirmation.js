91dcb55ce26029d69e77044b272fb61943328615
Souvik <souvik131@gmail.com>
2018-11-02 12:51:06 +0530

    confirmation.js

--- external/confirmation.js
+++ external/confirmation.js
@@ -43,14 +43,14 @@ function main(req, res){
 
 						console.log(session)
 						console.log(refId)
-						let rep={
+						let model={
 							repo:{"tags.session":session,"callback": true},
 							response:session+"-payment-"+refId,
 							data:{
 								transactionRefId:false
 							}
 						}
-						sendExternalData(rep)
+						sendExternalData(model)
 						.then((data)=>{ 
 				            model.tags={}
 							return resolve(model)
