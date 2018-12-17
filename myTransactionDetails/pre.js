781c8901b717708b2282efe95e4bbb80809ed92d
rohanjubi <rohan@jubi.ai>
2018-12-17 14:22:04 +0530

    msg

--- myTransactionDetails/pre.js
+++ myTransactionDetails/pre.js
@@ -219,7 +219,7 @@ function transactionID(model){
 	return new Promise((resolve,reject)=>{
 		console.log("-------------------------txid1-----------")
 		let reply
-		let rorp = model.tags.txResObj.TransactionTyp
+		let rorp = model.tags.txResObj.TransactionType
 
 		if(rorp.includes("Redemption")){
 			rorp = "reedemed"
