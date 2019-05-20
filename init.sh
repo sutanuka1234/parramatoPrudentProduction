a14c17cc1dbf97acc4ac27da34832d4c948a2f74
Sutanuka <sutanukaformal@gmail.com>
2019-05-20 15:34:33 +0530

    branchchanged

--- init.sh
+++ init.sh
@@ -3,4 +3,4 @@ git add .
 echo "Please enter comment: "
 read comment
 git commit -m "$comment"
-git push origin master
+git push origin parramato
--- serverInit.sh
+++ serverInit.sh
@@ -1,3 +1,3 @@
-git pull origin master
+git pull origin parramato
 sudo pm2 restart prudentwebpara
 sudo pm2 logs prudentwebpara
