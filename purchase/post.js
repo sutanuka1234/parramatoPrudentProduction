5cc7a0a6995a6f327c7d252429584832f0412908
Sutanuka <sutanukaformal@gmail.com>
2019-06-29 18:11:33 +0530

    main

--- purchase/post.js
+++ purchase/post.js
@@ -111,7 +111,7 @@ let greeting = ["Hope you are doing great today", "Good to see you!", "Hope you 
 
 function main(req, res) {
 	// console.log(req.params.stage)
-	console.log(req.params.stage)
+	console.log(req.params.stage+"main")
 	var buttonStageArr = req.body.data.split("|||")
 	if (buttonStageArr.length == 2 && obj[buttonStageArr[0]]) {
 		req.body.data = buttonStageArr[1]
