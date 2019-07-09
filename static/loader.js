82455d2786758c0ed78bc831530e6147938e2971
Sutanuka <sutanukaformal@gmail.com>
2019-07-09 13:03:39 +0530

    bundletestcahnge

--- static/loader.js
+++ static/loader.js
@@ -132,7 +132,7 @@
         socket: "https://cdnjs.cloudflare.com/ajax/libs/socket.io/2.1.1/socket.io.js",
         responsiveVoice: "https://code.responsivevoice.org/responsivevoice.js",
         nluComponent: "https://unpkg.com/compromise@latest/builds/compromise.min.js",
-        bundle: "https://parramato.com/bot-view/parramatoPrudentProduction_571731615155/dev/js/bundle.test.js",
+        bundle: "https://parramato.com/bot-view/parramatoPrudentProduction_571731615155/dev/js/bundle.js",
         // bundle: "./js/bundle.test.js"
         jubiEvents:"./js/jubievents.js"
     });
